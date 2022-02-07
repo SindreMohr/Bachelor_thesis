2b6bccfa5325ce9af264a52124f3dce26020a066
Serenity39 <69771022+serenity39@users.noreply.github.com>
2022-02-07 14:03:16 +0100

    fix this shit pls

--- data_read.py
+++ data_read.py
@@ -23,19 +23,8 @@ halfhourly_dataset_directory = data_directory + "/halfhourly_dataset/halfhourly_
 daily_dataset_files = os.listdir(daily_dataset_directory)
 halfhourly_dataset_files = os.listdir(halfhourly_dataset_directory)
 
-def unix_time(timestamp):
-    values = timestamp.split(" ")
-    date = values[0].split("-")
-    year = int(date[0])
-    month = int(date[1])
-    day = int(date[2])
-    date = datetime.date(year, month, day)
-    clock = values[1].split(":")
-    hour = int(clock[0])
-    minute = int(clock[1])
-    second = int(0)
-    clock = datetime.time(hour, minute, second)
-    return datetime.datetime.combine(date, clock).timestamp()
+def int_type(x):
+    return datetime.datetime(x).astype(int)/10**9
 
 def load_data_into_dataframe(dataset_option):
     """Returns dataframe for given dataset_option"""
@@ -56,8 +45,8 @@ def load_data_into_dataframe(dataset_option):
     if dataset_files_csv:
         start = time.time()
         if index_opt == "tstp":
-            df = pd.concat((pd.read_csv(file, converters={'tstp': unix_time}) for file in dataset_files_csv), ignore_index=False)
-            #df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
+            #df = pd.concat((pd.read_csv(file, converters={'tstp': unix_time}) for file in dataset_files_csv), ignore_index=False)
+            df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
             #df["tstp"] = df["tstp"].apply(unix_time)
         else:
             df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
