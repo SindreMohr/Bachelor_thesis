11a1313ebe1462388255995a538abbc6e3a61359
Sindre Mohr <sr.mohr@stud.uis.no>
2022-02-07 12:42:31 +0100

    datetime fix

--- data_read.py
+++ data_read.py
@@ -7,6 +7,8 @@
 
 import pandas as pd
 import os
+import time
+import datetime
 
 # Saving project directory to return cur to directory after file reads
 project_directory = os.getcwd()
@@ -21,6 +23,19 @@ halfhourly_dataset_directory = data_directory + "/halfhourly_dataset/halfhourly_
 daily_dataset_files = os.listdir(daily_dataset_directory)
 halfhourly_dataset_files = os.listdir(halfhourly_dataset_directory)
 
+def unix_time(timestamp):
+    values = timestamp.split(" ")
+    date = values[0].split("-")
+    year = int(date[0])
+    month = int(date[1])
+    day = int(date[2])
+    date = datetime.date(year, month, day)
+    clock = values[1].split(":")
+    hour = int(clock[0])
+    minute = int(clock[1])
+    second = int(0)
+    clock = datetime.time(hour, minute, second)
+    return datetime.datetime.combine(date, clock).timestamp()
 
 def load_data_into_dataframe(dataset_option):
     """Returns dataframe for given dataset_option"""
@@ -39,8 +54,20 @@ def load_data_into_dataframe(dataset_option):
 
     # Check if there is actually data in the list before reading to dataframe
     if dataset_files_csv:
-        df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
+        start = time.time()
+        if index_opt == "tstp":
+            df = pd.concat((pd.read_csv(file, converters={'tstp': unix_time}) for file in dataset_files_csv), ignore_index=False)
+            #df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
+            #df["tstp"] = df["tstp"].apply(unix_time)
+        else:
+            df = pd.concat((pd.read_csv(file) for file in dataset_files_csv), ignore_index=False)
         os.chdir(project_directory)
+        print(df.head())
+        print(start - time.time())
     else:
         raise FileNotFoundError(f"No files available for dataset option: '{dataset_option}'")
+    
     return df
+
+if __name__ == '__main__':
+    load_data_into_dataframe("halfhourly")
