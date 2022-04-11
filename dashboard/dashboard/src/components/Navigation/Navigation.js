ed439737dc3316f7f83030ff812020928879a532
Sindre Mohr <sr.mohr@stud.uis.no>
2022-04-11 09:22:42 +0200

    pushing faulty Navigation

--- dashboard/dashboard/src/components/Navigation/Navigation.js
+++ dashboard/dashboard/src/components/Navigation/Navigation.js
@@ -1,13 +1,19 @@
 import './Navigation.css';
 
+import { useContext } from 'react';
+
+import {GlobalContext} from '../../contexts/GlobalContext';
+
 function Navigation() {
+
+  const { setPageOverlay } = useContext(GlobalContext);
+
   return (
     <header>
         <h1>Dashboard</h1>
         <nav>
           <ul>
-            <li><a href="#">HOME</a></li>
-            <li><a href="#">PROJECTS</a></li>
+            <li onClick={setPageOverlay("page")}>PROJECTS</li>
             <li><a href="#">ABOUT</a></li>
           </ul>
         </nav>
